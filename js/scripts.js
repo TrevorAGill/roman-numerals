39d5eed9749f16b28b997fbfc6f2eabc6749b146
Jessica Sheridan and Trevor Gill <trevor@>
2017-07-13 11:13:01 -0700

    Add 1 to baseNumber

--- js/scripts.js
+++ js/scripts.js
@@ -6,15 +6,37 @@ $(document).ready(function() {
     var baseNumbers = [ 1,5,10,50,100,500,1000];
     var symbols = ["I","V","X","L","C","D","M"];
 
-
-    function getRoman(inputNumber) {
+    function getBaseNumberSymbol(inputNumber) {
       var found = 0;
       for (var index=0; index < baseNumbers.length; index+=1) {
         if ((baseNumbers[index] === inputNumber) && !found ) {
           found = 1;
           return (symbols[index]);
-        } 
+        }
       };
+    };
+
+    function getBaseNumber(inputNumber) {
+      var found = 0;
+      for (var index=0; index < baseNumbers.length; index+=1) {
+        if ((baseNumbers[index] === inputNumber) && !found ) {
+          found = 1;
+          return (baseNumbers[index]);
+        }
+      };
+    };
+
+//getRoman returns Symbol
+    function getRoman(inputNumber) {
+      var answerString = "";
+      var numbersToAdd= [];
+       numbersToAdd.push((getBaseNumber(inputNumber)));
+       numbersToAdd.push(1);
+       numbersToAdd.forEach(function(numberToAdd) {
+          answerString = answerString + getBaseNumberSymbol(numberToAdd);
+       });
+       return answerString;
+
     };
 
 
