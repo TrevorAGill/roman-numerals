bbcadfe218397a9e2417b8ad91adf9dd9723f659
Jessica Sheridan and Trevor Gill <jessica@>
2017-07-13 10:39:09 -0700

    include roman numeral spec2, all baseNumbers defined with corresponding symbols and outputting.

--- js/scripts.js
+++ js/scripts.js
@@ -3,9 +3,18 @@ $(document).ready(function() {
 
     //alert("hi");
 
-    function getRoman(inputNumber) {
-      return "I";
+    var baseNumbers = [ 1,5,10,50,100,500,1000];
+    var symbols = ["I","V","X","L","C","D","M"];
 
+
+    function getRoman(inputNumber) {
+      var found = 0;
+      for (var index=0; index < baseNumbers.length; index+=1) {
+        if ((baseNumbers[index] === inputNumber) && !found ) {
+          found = 1;
+          return (symbols[index]);
+        } 
+      };
     };
 
 
